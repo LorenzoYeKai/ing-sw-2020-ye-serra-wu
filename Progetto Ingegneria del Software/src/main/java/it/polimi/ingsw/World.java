19294aef9a420f77fb48c213659855173c564646
Pepser <61716016+Pepser@users.noreply.github.com>
2020-03-23 19:17:48 +0100

    Classe world
    aggiunta metodi : leveldifference,canmove,isnext,isintab,getspace

--- Progetto Ingegneria del Software/src/main/java/it/polimi/ingsw/World.java
+++ Progetto Ingegneria del Software/src/main/java/it/polimi/ingsw/World.java
@@ -1,14 +1,49 @@
 package it.polimi.ingsw;
 
+import java.awt.image.ImageProducer;
+
 public class World {
+        static Space[][] tab;
+        tab = new Space[5][5];
 
         public World () {
-            World =new Space [5][5];
+            tab = new Space[5][5];
             for(int i=0;i<5;i++) {
                 for(int j=0;j<5;j++) {
-                    World[i][j]=new Space(i,j);
+                    tab[i][j]=new Space(i,j);
                 }
             }
+        }
+        public static Space getSpaces(int x, int y){
+            return tab [x][y] ;
+        }
 
-    }
+        public static boolean isInTab(int y, int x)
+        {
+            return y > -1 && y < 5 && x > -1 && x < 5;
+        }
+
+        public static boolean isNext(int xattuale, int yattuale, int xmovimento, int ymovimento)
+        {
+            for (int i=yattuale-1;i<yattuale+2;i=i+1){
+                for (int j=xattuale-1;i<xattuale+2;j=j+1){
+                    if(i==ymovimento && j==xmovimento)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static int levelDifference(int xattuale, int yattuale, int xmovimento, int ymovimento){
+            int k;
+            k=getSpaces(xattuale, yattuale).getLevel()-getSpaces(xmovimento, ymovimento).getLevel();
+            return k;
+        }
+
+        public static Boolean canMoveThere(int xattuale,int yattuale,int xmovimento,int ymovimento){
+            int k = levelDifference(xattuale, yattuale, xmovimento, ymovimento);
+            return ( isNext(xattuale,yattuale, xmovimento, ymovimento) && isInTab(ymovimento,xmovimento) && k>-2 && getSpaces(xmovimento,ymovimento).isOccupied() );
+        }
 }
+
+
