0b6d9fe812ca64ab914a1cde3bd0807d40f13a51
Pepser <61716016+Pepser@users.noreply.github.com>
2020-03-23 11:57:49 +0100

    Space concluso
    CLasse base Space

--- Progetto Ingegneria del Software/src/main/java/it/polimi/ingsw/Space.java
+++ Progetto Ingegneria del Software/src/main/java/it/polimi/ingsw/Space.java
@@ -1,5 +1,41 @@
 package it.polimi.ingsw;
 
 public class Space {
-    //qualcosa
+    public int x;
+
+    public int y;
+
+    public boolean occupiedByWorker;
+
+    public boolean occupiedByDome;
+
+    public boolean border;
+
+    public int level;
+
+    public int getLevel(){
+        return level;
+    }
+
+    public void adaLevel(){
+        level=level+1;
+    }
+
+    public Boolean isOccupiedByWorker() {
+        return occupiedByWorker;
+    }
+    public Boolean isBorder(){
+        /* scegliere come implementarlo funzione aggiuntiva */
+    }
+
+    public Boolean isOccupiedByDome(){
+        return occupiedByDome;
+    }
+
+    public Boolean isOccupied(){
+        return (occupiedByDome || occupiedByWorker)
+    }
+
+
+
 }
