03a75304cbeb6739c292d712a5e5622121ff5ff1
Kishin98 <Kishin98@users.noreply.github.com>
2020-03-24 19:35:13 +0100

    Correzioni di battitura
    Tolta la classe God e corretto alcuni errori di battitura in Player.
    IMPORTANTE: implementare factory method per Worker

--- Progetto Ingegneria del Software/src/main/java/it/polimi/ingsw/Player.java
+++ Progetto Ingegneria del Software/src/main/java/it/polimi/ingsw/Player.java
@@ -4,66 +4,71 @@ import java.util.Scanner;
 
 public class Player {
 
-        private String name;
+    private String name;
 
-        private God god;
+    private Player nextPlayer;
 
-        private Player nextPlayer;
+    private boolean defeat;
 
-        private Boolean defeat;
+    static Worker[] workers = new Worker[2];
 
-       static Worker[] workers = new Worker[2];
+    public Player() {
+        setName();
+        chooseCard();
+        setWorkers();
+    }
 
-       public Player
 
-        {
-                setName();
-                chooseCard();
-                setWorkers();
-                }
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName() {
+        System.out.println("Choose a name");
+        Scanner Tastiera = new Scanner(System.in);
+        this.name = Tastiera.nextLine();
+    }
+
+    private void chooseCard() {
+        System.out.println("Choose a card");
+        Scanner Tastiera = new Scanner(System.in);
+        God temponary = Tastiera.nextLine();
+        while (!Game.isAvaiableGod(temponary)) {   // mi serve un metodo per vedere se il potere scelto è fra quelli possbili
+            System.out.println("error");
+            System.out.println("Choose a god");
+            God temponary = Tastiera.nextLine();
         }
+        this.god = temponary;
+    }
 
-        public String getName() { return this.name; }
+    public void setWorkers() {
 
-        private void setName() {
-               System.out.println("Choose a name");
-                Scanner Tastiera = new Scanner(System.in);
-                this.name = Tastiera.nextLine();
-       }
-
-        private void chooseCard() {
-                System.out.println("Choose a card");
-                Scanner Tastiera = new Scanner(System.in);
-                God temponary = Tastiera.nextLine();
-                while (!Game.isAvaiableGod(temponary)) {   // mi serve un metodo per vedere se il potere scelto è fra quelli possbili
-                        System.out.println("error");
-                        System.out.println("Choose a god");
-                        God temponary = Tastiera.nextLine();
-                }
-                this.god = temponary;
+        for (int i = 0; i < 2; i = i + 1) {
+            System.out.println("Choose  Worker position");
+            Scanner Tastiera = new Scanner(System.in);
+            int tempx = Tastiera.nextInt();
+            int tempy = Tastiera.nextInt();
+            while (World.getSpaces(tempx, tempy).isOccupied() || !World.isInWorld(tempx, tempy)) {
+                System.out.println("error,Choose Worker position");
+                tempx = Tastiera.nextInt();
+                tempy = Tastiera.nextInt();
+                World.getSpaces(tempx, tempy).setOccupiedByWorker();
+            }
+            workers[i] = new Worker(tempx, tempy);
         }
+    }
 
-        public void setWorkers() {
+    public Worker selectWorker(int k) {
+        return workers[k];
+    }
 
-                for (int i = 0; i < 2; i = i + 1) {
-                        System.out.println("Choose  Worker position");
-                        Scanner Tastiera = new Scanner(System.in);
-                        int tempx = Tastiera.nextInt();
-                        int tempy = Tastiera.nextInt();
-                        while (World.getSpaces(tempx, tempy).isOccupied() || !World.isInWorld(tempx, tempy)) {
-                                System.out.println("error,Choose Worker position");
-                                tempx = Tastiera.nextInt();
-                                tempy = Tastiera.nextInt();
-                                World.getSpaces(tempx, tempy).setOccupiedByWorker();
-                        }
-                        workers[i] = new Worker(tempx, tempy);
-                }
-        }
+    public God getGod() {
+        return this.god;
+    }
 
-        public Worker selectWorker(int k) { return workers[k]; }
-
-        public God getGod() { return this.god; }
-
-        public Boolean isDefeat()  { return (!World.canMove(selectWorker(0).getX(),selectWorker(1).getY())); }
+    public Boolean isDefeat() {
+        return (!World.canMove(selectWorker(0).getX(), selectWorker(1).getY()));
+    }
+}
 
 
