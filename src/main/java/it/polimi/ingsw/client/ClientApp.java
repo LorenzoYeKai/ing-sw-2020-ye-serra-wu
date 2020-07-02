f0bce1b21ce570b9691fd0930d9bc3c23a74cf59
lanyi <lanyi@ra3.moe>
2020-07-02 18:24:21 +0200

    add gui for jar

--- src/main/java/it/polimi/ingsw/client/ClientApp.java
+++ src/main/java/it/polimi/ingsw/client/ClientApp.java
@@ -1,12 +1,20 @@
 package it.polimi.ingsw.client;
 
+import it.polimi.ingsw.GUI.GUIApp;
+
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 public class ClientApp {
 
     public static void main(String[] args) {
+        if(List.of(args).contains("gui")) {
+            GUIApp.main(args);
+            return;
+        }
+
         Scanner input = new Scanner(System.in);
         String defaultIP = "127.0.0.1";
         System.out.println("Type Server IP (defaults to " + defaultIP + ")");
